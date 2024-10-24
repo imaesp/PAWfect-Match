cdcf204dddf3c73d28cca2cdfb14de30ca85ad7f
imaesp <127232789+imaesp@users.noreply.github.com>
2024-10-24 11:54:13 -0400

    Optimized setup

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.scss'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+        <h1 className='title'>PAWfect Match</h1>
+        </div>
+
+        <div>
+            <ul>
+                <li>
+                    <button>About</button>
+                </li>
+                <li>
+                    <button>Contact</button>
+                </li>
+                <li>
+                    <button>Services</button>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar.jsx'
+import Feed from '../../components/Feed/Feed.jsx'
+
+function Hero() {
+  return (
+    <section>
+        <Navbar></Navbar>
+        <Feed></Feed>
+    </section>
+  )
+}
+
+export default Hero
