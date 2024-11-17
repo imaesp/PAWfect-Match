110255a470b0aa60c4f82ec7318eae7d73e0daa6
LeyannaD <96096629+LeyannaD@users.noreply.github.com>
2024-11-17 16:01:09 -0500

    Footer and Nav Completed

--- frontend/src/components/Navbar/Dropdown.jsx
+++ frontend/src/components/Navbar/Dropdown.jsx
@@ -1,33 +1,24 @@
-
 import './Dropdown.scss';
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MenuItems } from './MenuItems';
 
-function Dropdown(props) {
-    const [isOpen, setIsOpen] = useState(false); //current state is not open until we setIsOpen
-
-    const handleHover = (isHovering) => {
-        setIsOpen(isHovering);
-    }
- 
-
+function Dropdown({ category }) {
     return (
-        <li 
-            className="dropdown" 
-            onMouseEnter={() => handleHover(true)} 
-            onMouseLeave={() => handleHover(false)}
-        >
-        <span className={`dropdown-title ${props.active ? 'active' : ''}`}>
-            {props.title}
-        </span> 
-        {isOpen && ( 
-            <ul className="dropdown-list">
-                {props.items.map((item, index) => (
-                <li key={index}>{item}</li>
-                ))}
-            </ul>
-        )}
-    </li>
+      <ul className="dropdown-menu">
+        <div className={`menu-category ${category}`}>
+          {MenuItems.filter(item => item.category === category).map((item, index) => (
+            <>
+             <li key={index}>
+              <Link className={item.cName} to={item.path}>
+                {item.title}
+              </Link>
+            </li> 
+
+            </>
+          ))}
+        </div>
+      </ul>
     );
 }
   
-export default Dropdown
+export default Dropdown;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,41 +1,56 @@
 import './Navbar.scss';
 import Dropdown from './Dropdown';
+import Button from './Button';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
-
 function Navbar() {
-  const [activeDropdown, setActiveDropdown] = useState('');
+  const [click, setClick] = useState(false);
+  const [hoveredCategory, setHoveredCategory] = useState(null); // Track the hovered category
 
-  const handleSetActive = (title) => {
-      setActiveDropdown(title);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = (category) => {
+    setHoveredCategory(category);
   };
 
-    return (
+  const onMouseLeave = () => {
+    setHoveredCategory(null);
+  };
+
+  return (
+    <>
       <nav className="navbar">
-          <div className="nav-logo">
-              <a href="#"><img src="src/components/Navbar/pawfectlogo.png" alt="PAWfect Match Logo" /></a>
-          </div>
-          <div className="page-links">
-            <ul>
-                <li>
-                  <a href ="#" onClick={() => handleSetActive('Home')}> 
-                    <Dropdown title="Home" items={['Quiz', 'Test']} />
-                  </a>
-                </li>
-                <li>
-                  <a href ="#" onClick={() => handleSetActive('About')}>  
-                    <Dropdown title="About" items={['Trips', 'Hub']} />
-                  </a>
-                </li>
-                <li>
-                  <a href ="#" onClick={() => handleSetActive('About')}> 
-                    <Dropdown title="Contact" items={['Insta', 'Facebook']} />
-                  </a>
-                </li>
-            </ul>
+        <Link to="/" className="nav-logo">
+          <img src="src/components/Navbar/pawfectlogo.png" alt="PAWfect Match Logo" />
+        </Link>
+
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
 
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          {/* Hover effect for each category */}
+          {['home', 'about', 'adapt'].map((category) => (
+            <li
+              key={category}
+              className="nav-item"
+              onMouseEnter={() => onMouseEnter(category)}
+              onMouseLeave={onMouseLeave}
+            >
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                {category.charAt(0).toUpperCase() + category.slice(1)}
+              </Link>
+              {/* Show the dropdown only if the category is hovered */}
+              {hoveredCategory === category && <Dropdown category={category} />}
+            </li>
+          ))}
+        </ul>
+        <Button />
       </nav>
-    );
+    </>
+  );
 }
+     
 export default Navbar;
