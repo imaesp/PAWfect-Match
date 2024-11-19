9536b46a8d012ac1c445c7d1f68d41cbf61029ce
imaesp <127232789+imaesp@users.noreply.github.com>
2024-11-18 21:24:46 -0500

    Update NearYou.jsx

--- frontend/src/pages/NearYou/NearYou.jsx
+++ frontend/src/pages/NearYou/NearYou.jsx
@@ -1,29 +1,6 @@
-<<<<<<< Updated upstream
+
 import React from 'react';
 import './NearYou.scss';
-
-const NearYou = () => {
-  return (
-    <section>
-      <div className="pet-gallery">
-      <h2>Pets Available Near You</h2>
-      <div className="pet-cards">
-        <div className="pet-card">
-          <img src="src\pages\NearYou\shooby-image.jpeg" alt="Shooby" />
-          <div className="pet-name">Shooby</div>
-        </div>
-        <div className="pet-card">
-          <img src="src\pages\NearYou\dooby-image.jpeg" alt="Dooby" />
-          <div className="pet-name">Dooby</div>
-        </div>
-        <div className="pet-card">
-          <img src="src\pages\NearYou\doo-image.avif" alt="Doo" />
-          <div className="pet-name">Doo</div>
-        </div>
-      </div>
-      <p>Meet the 9,000+ adoptable pets in your area!</p>
-    </div>
-=======
 import { useEffect, useState } from 'react';
 import supabase from '../../supabase/supabaseClient';
 import './NearYou.scss';
@@ -78,7 +55,6 @@ const NearYou = () => {
         </div>
         <p>Meet the 9,000+ adoptable pets in your area!</p>
       </div>
->>>>>>> Stashed changes
     </section>
   );
 };
