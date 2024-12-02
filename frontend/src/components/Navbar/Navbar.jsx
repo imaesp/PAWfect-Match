acd2e5e3f05c4fdc1a05bb692777ccc01e3c49bb
Sania Pervez <139937275+sanipervez@users.noreply.github.com>
2024-12-02 15:22:02 -0500

    Updated nav bar with application of bootstrap, reformating of code and style.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import Dropdown from './Dropdown';
 import Login from '../../components/Login/Login';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import leftpaw from '/Users/saniapervez/Desktop/Pawfect Match/PAWfect-Match/frontend/src/assets/leftpaw.png';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -23,30 +24,32 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-logo">
-        <img src="src/components/Navbar/pawfectlogo.png" alt="PAWfect Match Logo" />
-      </Link>
+       <div className="container text-center">
+        <div className="row justify-content-between align-items-center">
+          
+          <div className="col-auto">  
+            <img src= {leftpaw} alt="PAWfect Match logo" style={{ height: '50px', width: 'auto' }}/>
+          </div>
+          <div className="col-auto">
+            <Link to="/" className="nav-link"> PAWfect Match </Link>
+          </div>
 
-      <div className="menu-icon" onClick={handleClick}>
-        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          <div className='col-auto mid'>  
+            <div className="d-flex justify-content-end">
+              <Link to="/article" className="navLinks">Pet EDU</Link>
+              <Link to="/budget" className="navLinks">Budgeting Tool</Link>
+              <Link to="/adopt" className="navLinks">Adopt</Link>
+              <Link to="/survey" className="navLinks">Matchmaker</Link>
+              <Link to="/about" className="navLinks">About</Link>
+            </div>
+          </div>
+
+          <div className="col-auto login">
+            <Login />
+          </div>
+
+        </div>
       </div>
-
-      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-        {['home', 'about', 'adopt'].map((category) => (
-          <li
-            key={category}
-            className="nav-item"
-            onMouseEnter={() => onMouseEnter(category)}
-            onMouseLeave={onMouseLeave}
-          >
-            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-              {category.charAt(0).toUpperCase() + category.slice(1)}
-            </Link>
-            {hoveredCategory === category && <Dropdown category={category} />}
-          </li>
-        ))}
-      </ul>
-      <Login />
     </nav>
   );
 }
