dd84a1cc2c2c84b602a2c4b95fa79721e66e80ec
imaesp <127232789+imaesp@users.noreply.github.com>
2024-12-07 22:29:50 -0500

    updated images

--- frontend/src/components/CarouselAdopt/CarouselAdopt.jsx
+++ frontend/src/components/CarouselAdopt/CarouselAdopt.jsx
@@ -7,21 +7,21 @@ function CarouselAdopt(){
         <>
             <Carousel>
                 <Carousel.Item>
-                    <img src="src/components/CarouselAdopt/CarouselImage/CD2.webp" alt='Carousel Picture of animals 1' className='Img1'/>
+                    <img src="/CD2.webp" alt='Carousel Picture of animals 1' className='Img1'/>
                     <Carousel.Caption>
                         <h3>Adopt a new best friend</h3>
                         <p>Because every pet deserves a loving home.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                <img src='src/components/CarouselAdopt/CarouselImage/CD.png' alt='Carousel Picture of animals 2' className='Img2'/>
+                <img src='/CD.png' alt='Carousel Picture of animals 2' className='Img2'/>
                     <Carousel.Caption>
                         <h3>Whether it's a playful pup or a cuddly cat</h3>
                         <p> We have the PAW-PURRfect companion waiting for you =^.^=</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                <img src='src/components/CarouselAdopt/CarouselImage/CD3.jpg' alt='Carousel Picture of animals 3' className='Img3'/>
+                <img src='/CD3.jpg' alt='Carousel Picture of animals 3' className='Img3'/>
                     <Carousel.Caption>
                         <h3>Unite with your future furry family today</h3>
                         <p> pawsitively unforgettable bonds await!</p>
