fdf13c1aee3574c24666da7ed572e1bf9a535c8c
imaesp <127232789+imaesp@users.noreply.github.com>
2025-03-18 20:43:43 -0400

    Fixed conditional bug

--- frontend/src/pages/LandingSection/LandingSection.jsx
+++ frontend/src/pages/LandingSection/LandingSection.jsx
@@ -43,10 +43,10 @@ const LandingSection = () => {
           providing a personalized matchmaking service.
         </p>
       </div>
-      {(!data) ? (
-        <Card/>
-      ) : (
+      {(data) ? (
         <SignedCard/>
+      ) : (
+        <Card/>
       )}
     </div>
   );
