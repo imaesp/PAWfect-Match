ab51e81f1cbfa8b4f56d75043c1ebbe9f541601f
imaesp <127232789+imaesp@users.noreply.github.com>
2025-04-02 19:21:27 -0400

    Fetch organization by ID React Query functionality
    -Created and tested getOrganizationByID query
    -Created and tested useGetOrganizationByID Hook

---
+++ frontend/src/queries/getOrganizationByID.js
@@ -0,0 +1,23 @@
+export function getOrganizationByID(
+    client, 
+    orgID
+   ) {
+    return client
+      .from('organizations')
+      .select(`
+        orgID, 
+        name, 
+        city, 
+        state, 
+        address, 
+        country, 
+        zip, 
+        email, 
+        phone, 
+        orgurl
+      `)
+      .eq('orgID', orgID)
+      .throwOnError()
+      .maybeSingle();
+   }
+   
