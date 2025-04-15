78fa3cf72af9e83924857f325691d52f6e6eba20
imaesp <127232789+imaesp@users.noreply.github.com>
2025-04-15 18:10:26 -0400

    Fixed Card Component Logic

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -2,11 +2,20 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import useSurveyResponsesQuery from '../../hooks/useSurveyResponsesQuery.js';
+import { useUser } from '@clerk/clerk-react';
 import './Card.scss';
 
-
-
+// Clerk complains that we are already Logged in, so we cannot show the Login modal once we have a user.
+// if data ? Link to /survey : user ? Link to survey : Show Login Modal
 function Card() {
+
+  const { user } = useUser();
+  const { 
+    data, 
+    isLoading, 
+    isError 
+  } = useSurveyResponsesQuery(user?.id);
 
   return (
     <>
@@ -41,17 +50,32 @@ function Card() {
 
       {/* Call to Action */}
       <div className="row flex-column align-items-center mt-3">
-        <div className="col text-center flex-container"  style={{ borderTop: '2px solid black', margin: '10px 0', width: '250px' }}>
-          <SignInButton mode="modal">
-            <p role="button">Get Started</p>
-          </SignInButton>
-          <SignInButton mode="modal">
-            <button className="btn questionnaire_arrow">
-              <img style={{ width: '26px' }} src="/arrow_2.jpeg" className="img-fluid" alt="arrow to questionnaire" />
-            </button>
-          </SignInButton>
-          
-        </div>
+        {data ? (
+          <div className="col text-center flex-container"  style={{ borderTop: '2px solid black', margin: '10px 0', width: '250px' }}>
+              <Link to='/survey'><p role="button">Get Started</p></Link>
+              <Link to='/survey'><button className="btn questionnaire_arrow">
+                <img style={{ width: '26px' }} src="/arrow_2.jpeg" className="img-fluid" alt="arrow to questionnaire" />
+              </button></Link> 
+          </div>
+        ) : user ? (
+          <div className="col text-center flex-container"  style={{ borderTop: '2px solid black', margin: '10px 0', width: '250px' }}>
+              <Link to='/survey'><p role="button">Get Started</p></Link>
+              <Link to='/survey'><button className="btn questionnaire_arrow">
+                <img style={{ width: '26px' }} src="/arrow_2.jpeg" className="img-fluid" alt="arrow to questionnaire" />
+              </button></Link> 
+          </div>
+        ) : (
+          <div className="col text-center flex-container"  style={{ borderTop: '2px solid black', margin: '10px 0', width: '250px' }}>
+            <SignInButton mode="modal">
+              <Link><p role="button">Get Started</p></Link>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Link><button className="btn questionnaire_arrow">
+                <img style={{ width: '26px' }} src="/arrow_2.jpeg" className="img-fluid" alt="arrow to questionnaire" />
+              </button></Link> 
+            </SignInButton>
+          </div>
+        )}
       </div>
     </div>
    </>
