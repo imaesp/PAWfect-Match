bb7bd58b84c02fdc63e7f46afc49fb9048fad665
imaesp <127232789+imaesp@users.noreply.github.com>
2025-04-17 17:36:50 -0400

    Added pagination to Articles Page

--- frontend/src/components/Articles/articleFeed.jsx
+++ frontend/src/components/Articles/articleFeed.jsx
@@ -7,6 +7,7 @@ const defaultImage = 'path/to/default-image.jpg';
 const ArticleList = () => {
   const [imageError, setImageError] = useState({});
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
 
   const handleImageError = (index) => {
     setImageError((prevState) => ({ ...prevState, [index]: true }));
@@ -21,6 +22,23 @@ const ArticleList = () => {
     article.title.toLowerCase().includes(searchTerm) ||
     article.description.toLowerCase().includes(searchTerm)
   );
+
+  const itemsPerPage = 20;
+  const startIndex = (page - 1) * itemsPerPage;
+  const paginatedArticles = filteredArticles.slice(startIndex, startIndex + itemsPerPage);
+
+  const handlePageChange = (direction) => {
+      setPage((prevPage) => {
+          const totalPages = Math.ceil(filteredArticles.length / itemsPerPage);
+          if (direction === 'next' && prevPage < totalPages) {
+              return prevPage + 1;
+          } else if (direction === 'prev' && prevPage > 1) {
+              return prevPage - 1;
+          }
+          return prevPage;
+      });
+  };
+
 
   return (
     <div className="article-grid container mt-4">
@@ -40,8 +58,8 @@ const ArticleList = () => {
 
       {/* Article list */}
       <div className="article-list">
-        {filteredArticles.length > 0 ? (
-          filteredArticles.map((article, index) => {
+        {paginatedArticles.length > 0 ? (
+          paginatedArticles.map((article, index) => {
             const imageUrl = imageError[index]
               ? defaultImage
               : article.image_url || defaultImage;
@@ -65,11 +83,22 @@ const ArticleList = () => {
                   Read More
                 </a>
               </div>
+              
             );
           })
         ) : (
           <p className="no-results">No articles found matching your search.</p>
         )}
+      </div>
+       {/* Pagination Controls */}
+      <div className="pagination-controls">
+          <button className='pagination-button' onClick={() => handlePageChange('prev')} disabled={page === 1}>
+              Previous
+          </button>
+          <span>Page {page}</span>
+          <button className='pagination-button' onClick={() => handlePageChange('next')} disabled={page * itemsPerPage >= (filteredArticles).length}>
+              Next
+          </button>
       </div>
     </div>
   );
